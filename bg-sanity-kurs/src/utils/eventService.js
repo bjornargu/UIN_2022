b28dd77c15dc39c8fe1f2b9dba87769c324c4c7b
bjornargu <bjornar.guttormsen@gmail.com>
2022-03-16 17:08:44 +0100

    Finished sanity course

--- bg-sanity-kurs/src/utils/eventService.js
+++ bg-sanity-kurs/src/utils/eventService.js
@@ -12,3 +12,11 @@ export const getEvents = async () => {
   const data = await client.fetch(`*[_type == "event"]{${eventFields}}`)
   return data
 }
+
+export const getEvent = async (slug) => {
+  const data = await client.fetch(
+    `*[_type == "event" && slug.current == $slug]{${eventFields}}`,
+    { slug }
+  )
+  return data
+}
